96951eafa80ee8e1398d7d903f73605c36f576b0
gpt-pro-rep <134352270+gpt-pro-rep@users.noreply.github.com>
2023-05-27 21:56:14 +0800

    up

--- app/config/siteDesc.ts
+++ app/config/siteDesc.ts
@@ -1,26 +1,30 @@
 import React, { useEffect } from 'react';
 
 
-export const siteDesc = () => {
+export function siteDesc () {
+    let hostname:any = ''
+    
     useEffect(() => {
-        let hostname:any = location.hostname
+        hostname:any = location.hostname
         console.log(hostname)
-        hostname = hostname.split('.')
-        let alias:string = hostname[0]
-
-        switch (alias) {
-            case 'gpt': return {
-                apiUrl: '',
-                proName: 'Open-GPT',
-                proDesc: '开放版·直连GPT聊天机器人',
-                proPayUrl: 'https://e.northviewer.cn/wp-content/uploads/2023/05/code.png',
-            };
-            case 'quick': return {
-                apiUrl: '',
-                proName: 'Quick-Chat',
-                proDesc: '闪答·直连版GPT聊天机器人',
-                proPayUrl: 'https://e.northviewer.cn/wp-content/uploads/2023/05/qc.png',
-            }
-        }
     }, []);
+
+    hostname = hostname.split('.')
+    let alias:string = hostname[0]
+
+    switch (alias) {
+        case 'gpt': return {
+            apiUrl: '',
+            proName: 'Open-GPT',
+            proDesc: '开放版·直连GPT聊天机器人',
+            proPayUrl: 'https://e.northviewer.cn/wp-content/uploads/2023/05/code.png',
+        };
+        case 'quick': return {
+            apiUrl: '',
+            proName: 'Quick-Chat',
+            proDesc: '闪答·直连版GPT聊天机器人',
+            proPayUrl: 'https://e.northviewer.cn/wp-content/uploads/2023/05/qc.png',
+        }
+    }
+   
 }
