7347b6f1094aac8b7f1ce4948e373376421061f3
gpt-pro-rep <134352270+gpt-pro-rep@users.noreply.github.com>
2023-05-27 20:24:37 +0800

    type change

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 import {siteDesc} from './config/siteDesc'
-const siteDescConfig = siteDesc()
+const siteDescConfig:any = siteDesc()
 
 export const metadata = {
   title: siteDescConfig.proName,
--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -1,6 +1,6 @@
 import { SubmitKey } from "../store/config";
 import {siteDesc} from '../config/siteDesc'
-const siteDescConfig = siteDesc()
+const siteDescConfig:any = siteDesc()
 
 const cn = {
   WIP: "该功能仍在开发中……",
