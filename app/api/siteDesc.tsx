97ad54f0498e72cacb74ec0e9ad3c8badcb4971c
gpt-pro-rep <134352270+gpt-pro-rep@users.noreply.github.com>
2023-05-27 23:10:41 +0800

    up

--- app/api/siteDesc.tsx
+++ app/api/siteDesc.tsx
@@ -17,6 +17,12 @@ export const siteDesc = () => {
             proName: 'Quick-Chat',
             proDesc: '闪答·直连版GPT聊天机器人',
             proPayUrl: 'https://e.northviewer.cn/wp-content/uploads/2023/05/qc.png',
+        };
+        default: return {
+            apiUrl: '',
+            proName: 'Open-GPT',
+            proDesc: '开放版·直连GPT聊天机器人',
+            proPayUrl: 'https://e.northviewer.cn/wp-content/uploads/2023/05/code.png',
         }
     }
    
